6ed531c446438bf0fa1f0ef2adfd7f1a65147ed1
VadimYefimchuk <38383726+VadimYefimchuk@users.noreply.github.com>
2019-03-28 14:02:45 +0200

    Add files via upload

---
+++ Lab1/Lab!/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Threading;
+
+
+class ThreadSafe
+{
+    static bool done;
+    static object locker = new object();
+
+    static void Main(string[] args)
+    {
+        new Thread(Go).Start();
+        Go();
+        Console.ReadLine();
+    }
+
+    static void Go()
+    {
+        lock (locker)
+        {
+            if (!done)
+            {
+                Console.Write("Done");
+                done = true;
+            }
+        }
+    }
+}
---
+++ Lab1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Threading;
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        new Thread(Go).Start();
+        Go();
+    }
+
+    static void Go()
+    {
+        for (int i = 0; i < 5; i++)
+            Console.Write('P');
+    }
+}
+
